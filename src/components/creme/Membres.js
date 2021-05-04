619f817f530b6b2b8f867a8ac696682cfd71ea14
Azzouz-ichrak <ichrak12.azzouz@gmail.com>
2021-05-04 16:14:27 +0100

    modifier partie membres du bureau

--- src/components/creme/Membres.js
+++ src/components/creme/Membres.js
@@ -5,10 +5,106 @@ import Col from 'react-bootstrap/Col'
 import underTitle from '../images/under-title.png'
 import membre1 from '../images/membre1.png'
 import lien from '../images/lien-linkedin.png'
+import team_01 from '../images/team/team_01.jpg'
+import team_02 from '../images/team/team_02.jpg'
+import team_03 from '../images/team/team_03.jpg'
+import team_04 from '../images/team/team_04.jpg'
+import linkedin from '../images/linkedin.png'
 
 const Membres = () => {
     return (
         <div className="section-membres">
+        <Container fluid id="team">
+            <Container>
+            <Row><h2>Membre Du Bureau Exécutif</h2></Row>
+            <Row>
+            <Col xl="3" lg="3" md="6" sm="12"ms="12" className="one-team">
+                   <img src={team_01} alt="Team metco" />
+                    <div className="descr">
+                        <h4>John Portman</h4>
+                        <p>Manager</p>
+                        <span className="social-media">
+                            <img src={linkedin} alt="linkedin" />
+                        </span>
+                    </div>
+                </Col>
+                <Col xl="3" lg="3" md="6" sm="12"ms="12" className="one-team">
+                   <img src={team_02} alt="Team metco" />
+                    <div className="descr">
+                        <h4>John Portman</h4>
+                        <p>Manager</p>
+                        <span className="social-media">
+                            <img src={linkedin} alt="linkedin" />
+                        </span>
+                    </div>
+                </Col>
+                <Col xl="3" lg="3" md="6" sm="12"ms="12" className="one-team">
+                   <img src={team_03} alt="Team metco" />
+                    <div className="descr">
+                        <h4>John Portman</h4>
+                        <p>Manager</p>
+                        <span className="social-media">
+                            <img src={linkedin} alt="linkedin" />
+                        </span>
+                    </div>
+                </Col>
+                <Col xl="3" lg="3" md="6" sm="12"ms="12" className="one-team">
+                   <img src={team_04} alt="Team metco" />
+                    <div className="descr">
+                        <h4>John Portman</h4>
+                        <p>Manager</p>
+                        <span className="social-media">
+                            <img src={linkedin} alt="linkedin" />
+                        </span>
+                    </div>
+                </Col>
+            
+                <Col xl="3" lg="3" md="6" sm="12"ms="12" className="one-team">
+                   <img src={team_01} alt="Team metco" />
+                    <div className="descr">
+                        <h4>John Portman</h4>
+                        <p>Manager</p>
+                        <span className="social-media">
+                            <img src={linkedin} alt="linkedin" />
+                        </span>
+                    </div>
+                </Col>
+                <Col xl="3" lg="3" md="6" sm="12"ms="12" className="one-team">
+                   <img src={team_02} alt="Team metco" />
+                    <div className="descr">
+                        <h4>John Portman</h4>
+                        <p>Manager</p>
+                        <span className="social-media">
+                            <img src={linkedin} alt="linkedin" />
+                        </span>
+                    </div>
+                </Col>
+                <Col xl="3" lg="3" md="6" sm="12"ms="12" className="one-team">
+                   <img src={team_03} alt="Team metco" />
+                    <div className="descr">
+                        <h4>John Portman</h4>
+                        <p>Manager</p>
+                        <span className="social-media">
+                            <img src={linkedin} alt="linkedin" />
+                        </span>
+                    </div>
+                </Col>
+                <Col xl="3" lg="3" md="6" sm="12"ms="12" className="one-team">
+                   <img src={team_04} alt="Team metco" />
+                    <div className="descr">
+                        <h4>John Portman</h4>
+                        <p>Manager</p>
+                        <span className="social-media">
+                            <img src={linkedin} alt="linkedin" />
+                        </span>
+                    </div>
+                </Col>
+            </Row>
+            </Container>
+            
+        </Container>
+   
+            { /** 
             <Container>
                 <Row ><h2>Membre Du Bureau Exécutif</h2></Row>
                 <Row> <img src={underTitle} /></Row>
@@ -115,6 +211,10 @@ const Membres = () => {
 
                 </Row>
             </Container>
+*/}
+
+
+
         </div>
     )
 }
